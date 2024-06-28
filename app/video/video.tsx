87de06d6ca42f64409cf45fa46b6fc8a508a2c81
JojoYay <mobilejoz@gmail.com>
2024-06-28 16:40:11 +0800

    update page meta

---
+++ app/video/video.tsx
@@ -0,0 +1,72 @@
+'use client';
+import { useEffect, useState } from 'react';
+import VideoCard from './videoCard';
+import { CircularProgress, Grid, Pagination } from '@mui/material';
+import Head from 'next/head';
+
+export default function Video() {
+	useEffect(() => {
+		fetchVideo();
+	}, []);
+
+	const [responseData, setResponseData] = useState<string[][] | null>(null);
+	const [currentPage, setCurrentPage] = useState(1);
+	const itemsPerPage = 8;
+
+	const fetchVideo = async () => {
+		try {
+			const url = process.env.SERVER_URL + '?func=getVideo';
+			if (url) {
+				const response = await fetch(url, {
+					method: 'GET',
+				});
+				const data = await response.json();
+				// console.log(data.result);
+				setResponseData(data.result.slice(2).reverse()); // Reverse the order
+			}
+		} catch (error) {
+			console.error('Error fetching data:', error);
+		}
+	};
+
+	const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+		setCurrentPage(value);
+	};
+
+	const startIndex = (currentPage - 1) * itemsPerPage;
+	const endIndex = startIndex + itemsPerPage;
+	const currentItems = responseData ? responseData.slice(startIndex, endIndex) : [];
+
+	return (
+		<>
+			{responseData ? (
+				<>
+					<Pagination
+						count={Math.ceil(responseData.length / itemsPerPage)}
+						page={currentPage}
+						onChange={handlePageChange}
+						style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}
+					/>				
+					<Grid container spacing={2} style={{ margin: '5px', width:'100%' }}>
+						{currentItems.map((data, index) => (
+							<div key={index}>
+								<VideoCard url={data[2]} title={data[1]} date={data[0].substring(0, 10)}></VideoCard>
+							</div>
+						))}
+					</Grid>
+					<Pagination
+						count={Math.ceil(responseData.length / itemsPerPage)}
+						page={currentPage}
+						onChange={handlePageChange}
+						style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}
+					/>
+				</>
+			) : (
+				<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+					<div>Loading...</div>
+					<CircularProgress />
+				</div>
+			)}
+		</>
+	);
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // env: {
-  //   LIFF_ID: "2005609302-AQql19g6",
-  //   SERVER_URL: "https://script.google.com/macros/s/AKfycbw7wL2okZJ8z9Vv7G4nQS3asz-pEmoRSJrNMKmI50p2ZsegfcyxWAUwxoVcAHiiYIQWZA/exec"
-  // },
   env: {
     LIFF_ID: process.env.NEXT_PUBLIC_LIFF_ID,
     SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
